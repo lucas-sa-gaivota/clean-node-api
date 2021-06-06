d75d19ae494faac340d7b74e03bb10b16a9df7f4
Lucas Sá Nascimento <lucassnascimento20@gmail.com>
2021-06-06 14:17:14 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true
+}
+
+module.exports = config
+module.exports = async () => {
+  return {
+    verbose: true
+  }
+}
