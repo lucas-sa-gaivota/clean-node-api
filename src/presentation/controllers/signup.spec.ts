b2f0629c9c585aa2090f39f82bba804774c3a6aa
Lucas Sá Nascimento <lucassnascimento20@gmail.com>
2021-06-11 22:52:21 -0300

    test: ensure reatuns 400 if no provided email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,7 +12,21 @@ describe('SignUp Controler', () => {
       }
     }
     const httpResponse = sut.handle(httpRequest)
-    // expect(httpResponse.statusCode).toBe(400)
-    expect(1).toBe(1)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
